ac4640c803b0454298b7a877349bf0191f3d7d4f
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-04 12:28:40 -0600

    add song page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -15,11 +15,33 @@ class PageController < ApplicationController
   
 # The page should return 6 randomly generated numbers between 1 and 60 so as to predict the upcoming lotto numbers.
   def lottery_generator
-    lotto = []
+    lotto_numbers = []
     6.times do
-      lotto << Random.new.rand(1..60) 
+      lotto_numbers << Random.new.rand(1..60) 
     end
 
-    render json: {:lotto => lotto}
+    render json: {:lotto => lotto_numbers}
   end
+
+  
+# Every time a user visits/refreshes the page, the page should tell the user the number of visits made to that page so far. NOTE: This is impossible, but you should try your hand at this for a few minutes to discover why it’s impossible.
+  def page_counter
+    counter = 0 
+
+    render json: {:visits => counter}
+  end
+
+
+# Add a page that returns the lyrics for “99 bottles of beer on the wall”.
+  def lyrics
+    counter = 99
+    song_lyrics = []
+    99.times do
+      song_lyrics << "#{counter} bottles of beer on the wall"
+      counter -= 1
+    end
+    
+    render json: {sing: song_lyrics}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/fortune' => 'page#fortune_teller'
   get '/lottery' => 'page#lottery_generator'
+  get '/page' => 'page#page_counter'
+  get '/song' => 'page#lyrics'
 end
